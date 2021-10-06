f770addbb91773706bd7705c4e6883461caf794f
Anda Totilca <totilcaanda18@stud.ase.ro>
2021-10-06 05:14:44 +0000

    Add existing file

--- src/hello.js
+++ src/hello.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+// let sayHello = (name) => {
+//   return `Hello, ${name}!`;
+// };
+
+// console.log(sayHello(process.argv[2]));
+
+let concatenateStrings = (str) => {
+  let str1 = JSON.parse(str);
+  let str2 = "";
+  for (let i = 0; i < str1.length; i++) {
+    str2 = str2.concat(str1[i]);
+  }
+  return str2;
+};
+console.log(concatenateStrings(process.argv[2]));
